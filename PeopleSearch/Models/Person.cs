53412255c434da444b69ee8c27903b83d5aa4ca1
SushiGuy <jeremyheiner@gmail.com>
2017-08-10 22:55:02 -0700

    Project functional, next do unit tests
    Code first, ASP.NET MVC, SQL database, .NET Core, Seeded data (3 people)

---
+++ PeopleSearch/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeopleSearch.Models
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options) 
+        { }
+
+        public DbSet<Person> Person { get; set; }
+    }
+
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FullName { get; set; }
+
+        public string Address { get; set; }
+
+        public int Age { get; set; }
+
+        public string Interests { get; set; }
+    }
+}
---
+++ PeopleSearch/Models/SeedData.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleSearch.Models
+{
+    public class SeedData
+    {
+        private PersonContext _context;
+
+        public SeedData(PersonContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Seed()
+        {
+            if (!_context.Person.Any())
+            {
+                var obj1 = new Person()
+                {
+                    FullName = "Liz Parker",
+                    Address = "123 S St, Roswell NM 88202",
+                    Age = 25,
+                    Interests = "Aliens, Japanese language and culture",
+                };
+                _context.Person.Add(obj1);
+
+                var obj2 = new Person()
+                {
+                    FullName = "Max Evans",
+                    Address = "456 S St, Roswell NM 88203",
+                    Age = 27,
+                    Interests = "Science and Astronomy",
+                };
+                _context.Person.Add(obj2);
+
+                var obj3 = new Person()
+                {
+                    FullName = "Maria Deluca",
+                    Address = "789 S St, Roswell NM 88203",
+                    Age = 26,
+                    Interests = "English Literature",
+                };
+                _context.Person.Add(obj3);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
