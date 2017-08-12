c9962f7e4944d3d5b24b662d70ba5134a6f1f82b
SushiGuy <jeremyheiner@gmail.com>
2017-08-11 19:59:58 -0700

    Second controller test - Home/SearchResult/

--- PeopleSearch/Models/Person.cs
+++ PeopleSearch/Models/Person.cs
@@ -5,11 +5,13 @@ namespace PeopleSearch.Models
 {
     public class PersonContext : DbContext
     {
+        public PersonContext() { }
+
         public PersonContext(DbContextOptions<PersonContext> options)
             : base(options) 
         { }
 
-        public DbSet<Person> Person { get; set; }
+        public virtual DbSet<Person> Person { get; set; }
     }
 
     public class Person
